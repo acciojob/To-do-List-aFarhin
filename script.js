f773328a7a8684049803b8c74495b17573146b93
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-02 09:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here
 
+const newTodoInput = document.getElementById('newTodoInput');
+const addTodoBtn = document.getElementById('addTodoBtn');
+const todoList = document.getElementById('todoList');
+
+addTodoBtn.addEventListener('click', () => {
+  const newTodo = newTodoInput.value.trim(); // remove whitespace from start and end
+  if (newTodo !== '') {
+    const todoItem = document.createElement('li');
+    todoItem.textContent = newTodo;
+    todoList.appendChild(todoItem);
+    newTodoInput.value = '';
+  }
+});
+
+newTodoInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    const newTodo = newTodoInput.value.trim(); // remove whitespace from start and end
+    if (newTodo !== '') {
+      const todoItem = document.createElement('li');
+      todoItem.textContent = newTodo;
+      todoList.appendChild(todoItem);
+      newTodoInput.value = '';
+    }
+  }
+});
+
+
